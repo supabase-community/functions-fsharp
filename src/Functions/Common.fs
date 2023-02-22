bc767faf2b186015845e7094404c95fb682af8a0
Samo8 <samodubovec@gmail.com>
2023-02-22 08:04:42 +0100

    refactor: refactored connection headers and http

--- src/Functions/Common.fs
+++ src/Functions/Common.fs
@@ -1,23 +1,19 @@
 namespace Functions.Common
 
 open System
-open System.Collections.Generic
-open System.Net.Http
+open System.Net.Http.Headers
 
 [<AutoOpen>]
 module Common =
-    let private addRequestHeader (key: string) (value: string) (client: HttpClient): unit =
-        client.DefaultRequestHeaders.Add(key, value)
+    let internal addRequestHeaders (headers: Map<string, string>) (httpRequestHeaders: HttpRequestHeaders): unit =
+        headers |> Seq.iter (fun (KeyValue(k, v)) -> httpRequestHeaders.Add(k, v))
     
-    let internal addRequestHeaders (headers: IDictionary<string, string>) (client: HttpClient): unit =
-        headers |> Seq.iter (fun (KeyValue(k, v)) -> client |> addRequestHeader k v)
-        
-    let internal addBearerIfMissing (headers: IDictionary<string, string>) =
+    let internal addBearerIfMissing (headers: Map<string, string>) =
         let apiKey =
             match headers.TryGetValue "apiKey" with
             | true, key -> key
             | _         -> raise (Exception "Missing apiKey")
         
         match headers.ContainsKey "Authorization" with
-        | true  -> () 
-        | false -> headers.Add("Authorization", $"Bearer {apiKey}")
+        | true  -> headers
+        | false -> headers |> Map.add "Authorization" $"Bearer {apiKey}"
