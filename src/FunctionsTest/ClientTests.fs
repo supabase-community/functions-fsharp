8d263bbe4778e2810a8849182222bc7a67395729
Samo8 <samodubovec@gmail.com>
2023-04-25 11:45:59 +0200

    ci: fixed test not running on pipeline

--- src/FunctionsTest/ClientTests.fs
+++ src/FunctionsTest/ClientTests.fs
@@ -2,66 +2,20 @@
 
 open System.Net
 open System.Net.Http
-open System.Text
 open System.Threading
-open System.Threading.Tasks
-open Functions
+open FsUnit
 open FsUnit.Xunit
 open Moq
 open Moq.Protected
 open Xunit
-
-let mockHttpMessageHandlerWithBody (response: string) (requestBody: string) =
-        let mockHandler = Mock<HttpMessageHandler>(MockBehavior.Strict)
-        mockHandler.Protected()
-            .Setup<Task<HttpResponseMessage>>(
-                "SendAsync",
-                ItExpr.IsAny<HttpRequestMessage>(),
-                ItExpr.IsAny<CancellationToken>()
-            )
-            .Callback(fun (req: HttpRequestMessage) (_: CancellationToken) -> 
-                req.Content <- new StringContent(requestBody, Encoding.UTF8, "application/json")
-            )
-            .ReturnsAsync(
-                new HttpResponseMessage(
-                    HttpStatusCode.OK,
-                    Content = new StringContent(response, Encoding.UTF8, "application/json")
-                )
-            )
-            .Verifiable()
-        mockHandler
-        
-let mockHttpMessageHandlerWithBodyFail (error: FunctionsError) (requestBody: string) =
-        let mockHandler = Mock<HttpMessageHandler>(MockBehavior.Strict)
-        let statusCode = if error.statusCode.IsSome then error.statusCode.Value else HttpStatusCode.BadRequest
-        mockHandler.Protected()
-            .Setup<Task<HttpResponseMessage>>(
-                "SendAsync",
-                ItExpr.IsAny<HttpRequestMessage>(),
-                ItExpr.IsAny<CancellationToken>()
-            )
-            .Callback(fun (req: HttpRequestMessage) (_: CancellationToken) -> 
-                req.Content <- new StringContent(requestBody, Encoding.UTF8, "application/json")
-            )
-            .ReturnsAsync(
-                new HttpResponseMessage(
-                    statusCode,
-                    Content = new StringContent(error.message, Encoding.UTF8, "application/json")
-                )
-            )
-            .Verifiable()
-        mockHandler
-        
-type CustomResponse = {
-    message: string
-}
+open Functions
 
 [<Collection("invoke tests")>]
 module InvokeTests =
     [<Fact>]
     let ``should return a given type <'T> of response when success`` () =
         // Arrange
-        let expectedResponse = { message = "function invoked successfull" }
+        let expectedResponse = { message = "function invoked successfully" }
         let response ="""{"message":"function invoked successfully"}"""
         let requestBody = """{"name:"function-name"}"""
             
